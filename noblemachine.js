6f5622f9b08eb78e81f4d6cf73365f3ff243887d
Daniel Assange <daniel@noblesamurai.com>
2011-01-11 13:08:17 +1100

    Restructuring into a single closure, allowing multiple arguments to be passed between states.

--- noblemachine.js
+++ noblemachine.js
@@ -35,9 +35,249 @@ var NobleMachine = function(funcOrAct) {
 		});
 	}
 
-	me.addState('complete', function() {
-		var me = this;
+	/**
+	 * Internal function, converts a function or action into a state handler function.
+	 */
+	function makeHandler(funcOrAct) {
+		return function() {
+			var state = me.state;
 
+			if (funcOrAct === undefined) {
+				me.toNext();
+			} else if (funcOrAct instanceof Function) {
+				funcOrAct.apply(me, arguments);
+
+				if (!funcOrAct.wait && me.state == state && !me.transitioning && me.running)  {
+					me.toNext();
+				}
+			} else {
+				me.toNext(funcOrAct);
+			}
+		}
+	}
+
+	/**
+	 * Add a named state without placing it in the main sequence.
+	 */
+	function addState(name, funcOrAct) {
+		me.handlers[name] = me.makeHandler(funcOrAct);
+	}
+
+	/**
+	 * Add a named state at the next position in the main sequence.
+	 */
+	function nextState(name, funcOrAct) {
+		me.addState(name, funcOrAct);
+		me.stateSequence.push(name);
+	}
+
+	/**
+	 * Add an unnamed state at the next position in the main sequence.
+	 */
+	function next(funcOrAct) {
+		me.nextState("state" + this.stateSequence.length, funcOrAct);
+	}
+
+	/**
+	 * Assign a handler to be called when the machine has successfully completed.
+	 */
+	function complete(handler) {
+		me.completeHandler = handler;
+	}
+
+	/**
+	 * Assign a handler to be called when the machine encounters an error.
+	 */
+	function error(handler) {
+		me.errorHandler = handler;
+	}
+
+	/**
+	 * Add a function to be run on machine exit regardless of exit status.
+	 */
+	function ensure(handler) {
+		me.exitHandler = handler;
+	}
+
+	/**
+	 * Called on final exit.
+	 */
+	function onExit() {
+		if (me.exitHandler) me.exitHandler();
+	}
+
+	/**
+	 * Retrieve a state name by main sequence index.
+	 */
+	function findState(nameOrIndex) {
+		if (typeof nameOrIndex == 'string') {
+			return nameOrIndex;
+		} else {
+			return me.stateSequence[nameOrIndex];
+		}
+	}
+
+	/**
+	 * Run the handler for the current state.
+	 */
+	function go(args) {
+		try {
+			me.handlers[me.state].apply(me, args);
+		} catch (e) {
+			if (e.stack) NobleMachine.logger.error(e.stack);
+			if (me.state == 'error') {
+				me.emitError(e);
+			} else {
+				me.toError(e);
+			}
+		}
+	}
+
+	/**
+	 * Switch to a given state and proceed.
+	 */
+	function runState(nameOrIndex, args) {
+		me.state = me.findState(nameOrIndex);
+
+		var index = me.stateSequence.indexOf(me.state);
+		if (index != -1) me.stateIndex = index;
+
+
+		if (args) {
+			me.go(args);	
+		} else {
+			me.go();
+		}
+	}
+
+	/**
+	 * Add a state transition involving an action and success/error response states.
+	 */
+	function transition(opts) {
+		var e = this;
+
+		opts = opts || {};
+		opts.error = opts.error || 'error';
+		opts.success = opts.success || 'complete';
+
+		if (undefined == opts.action) {
+			throw 'Transition added with no action';
+		}
+
+		var act = opts.action;
+
+
+		act.addListener('success', function() {
+			me.transitioning = false;
+			me.runState(opts.success, (opts.argdata||[]).concat(arguments));
+		});
+
+		act.addListener('error', function() {
+			me.transitioning = false;
+			me.runState(opts.error, arguments);
+		});
+
+		me.transitioning = true;
+		act.start();
+	}
+
+	/**
+	 * Transition directly to the given state with the given action or variable.
+	 */
+	function toState(nameOrIndex) {
+		var args = Array.prototype.slice.call(arguments, 1);
+		var state = me.findState(nameOrIndex);
+
+		if (args.length == 0) {
+			me.runState(state);
+		} else if (args[0] && args[0].start instanceof Function) {
+			me.transition({ action: actOrVar, success: state, argdata: args.slice(1) });
+		} else {
+			me.runState(state, args);
+		}
+	}
+
+	/**
+	 * Transition to the next state with the given action or variable.
+	 */
+	function toNext() {
+		var nextState = me.stateIndex+1;
+		var args = Array.prototype.slice.call(arguments);
+
+		if (nextState < me.stateSequence.length) {
+			me.toState.apply(me, [nextState].concat(args));
+		} else {
+			me.toComplete.apply(me, args);
+		}
+	}
+
+	/**
+	 * As above, but to the previous state.
+	 */
+	function toPrev() {
+		var nextState = me.stateIndex-1;
+		var args = Array.prototype.slice.call(arguments);
+
+		if (nextState < 0) {
+			me.toError("Tried to transition earlier than the first state?!");
+		} else {
+			me.toState.apply(me, [nextState].concat(args));
+		}
+	}
+
+	/**
+	 * Transition to the current state (i.e repeat it).
+	 */
+	function toRepeat() {
+		me.toState(me, [me.stateIndex].concat(Array.prototype.slice.call(arguments)));
+	}
+
+	/**
+	 * Transition straight to completion.
+	 */
+	function toComplete() {
+		me.toState.apply(this, ['complete'].concat(
+			Array.prototype.slice.call(arguments)));
+	}
+
+	/**
+	 * Transition to error handler.
+	 */
+	function toError() {
+		me.toState.apply(this, ['error'].concat(
+			Array.prototype.slice.call(arguments)));
+	}
+
+	/**
+	 * Emit success event and mark machine as finished.
+	 */
+	function emitSuccess() {
+		me.running = false;
+		me.emit.apply(me, ['success'].concat(
+			Array.prototype.slice.call(arguments)));
+	}
+
+	/** 
+	 * Emit error event and mark machine as finished.
+	 */
+	function emitError() {
+		me.running = false;
+		me.emit.apply(me, ['error'].concat(
+			Array.prototype.slice.call(arguments)));
+	}
+
+	['makeHandler', 'addState', 'nextState', 'next', 'complete', 'error', 'ensure', 'onExit', 'findState', 'go', 
+	 'runState', 'transition', 'toState', 'toNext', 'toPrev', 'toRepeat', 'toComplete', 'toError', 'emitSuccess', 
+	 'emitError'].forEach(function(funcName) {
+		 me[funcName] = eval(funcName);
+	});
+
+	me.next.wait = function(handler) {
+		handler.wait = true;
+		me.next(handler);
+	}
+
+	me.addState('complete', function() {
 		if (me.completeHandler) {
 			me.completeHandler.apply(me, arguments);
 		} 
@@ -50,8 +290,6 @@ var NobleMachine = function(funcOrAct) {
 	});
 
 	me.addState('error', function() {
-		var me = this;
-
 		if (me.errorHandler) {
 			me.errorHandler.apply(me, arguments);
 		}
@@ -71,250 +309,6 @@ var NobleMachine = function(funcOrAct) {
 }
 
 sys.inherits(NobleMachine, events.EventEmitter);
-
-/**
- * Internal function, converts a function or action into a state handler function.
- */
-NobleMachine.prototype.makeHandler = function(funcOrAct) {
-	var me = this;
-
-	return function() {
-		var state = me.state;
-
-		if (funcOrAct === undefined) {
-			me.toNext();
-		} else if (funcOrAct instanceof Function) {
-			funcOrAct.apply(me, arguments);
-
-			if (me.state == state && !me.transitioning && me.running)  {
-				me.toNext();
-			}
-		} else {
-			me.toNext(funcOrAct);
-		}
-	}
-}
-
-/**
- * Add a named state without placing it in the main sequence.
- */
-NobleMachine.prototype.addState = function(name, funcOrAct) {
-	this.handlers[name] = this.makeHandler(funcOrAct);
-}
-
-/**
- * Add a named state at the next position in the main sequence.
- */
-NobleMachine.prototype.nextState = function(name, funcOrAct) {
-	this.addState(name, funcOrAct);
-	this.stateSequence.push(name);
-}
-
-/**
- * Add an unnamed state at the next position in the main sequence.
- */
-NobleMachine.prototype.next = function(funcOrAct) {
-	this.nextState("state" + this.stateSequence.length, funcOrAct);
-}
-
-/**
- * Assign a handler to be called when the machine has successfully completed.
- */
-NobleMachine.prototype.complete = function(handler) {
-	this.completeHandler = handler;
-}
-
-/**
- * Assign a handler to be called when the machine encounters an error.
- */
-NobleMachine.prototype.error = function(handler) {
-	this.errorHandler = handler;
-}
-
-/**
- * Add a function to be run on machine exit regardless of exit status.
- */
-NobleMachine.prototype.ensure = function(handler) {
-	this.exitHandler = handler;
-}
-
-/**
- * Called on final exit.
- */
-NobleMachine.prototype.onExit = function() {
-	if (this.exitHandler) this.exitHandler();
-}
-
-/**
- * Retrieve a state name by main sequence index.
- */
-NobleMachine.prototype.findState = function(nameOrIndex) {
-	var me = this;
-
-	if (typeof nameOrIndex == 'string') {
-		return nameOrIndex;
-	} else {
-		return me.stateSequence[nameOrIndex];
-	}
-}
-
-/**
- * Run the handler for the current state.
- */
-NobleMachine.prototype.go = function(args) {
-	var me = this;
-
-	try {
-		me.handlers[me.state].apply(me, args);
-	} catch (e) {
-		if (e.stack) NobleMachine.logger.error(e.stack);
-		if (me.state == 'error') {
-			me.emitError(e);
-		} else {
-			me.toError(e);
-		}
-	}
-}
-
-/**
- * Switch to a given state and proceed.
- */
-NobleMachine.prototype.runState = function(nameOrIndex, args) {
-	var me = this;
-
-	me.state = me.findState(nameOrIndex);
-
-	var index = me.stateSequence.indexOf(me.state);
-	if (index != -1) me.stateIndex = index;
-
-
-	if (args) {
-		me.go(args);	
-	} else {
-		me.go();
-	}
-}
-
-/**
- * Add a state transition involving an action and success/error response states.
- */
-NobleMachine.prototype.transition = function(opts) {
-    var me = this;
-
-    opts = opts || {};
-	opts.error = opts.error || 'error';
-	opts.success = opts.success || 'complete';
-
-    if (undefined == opts.action) {
-        throw 'Transition added with no action';
-    }
-
-	var act = opts.action;
-
-
-	act.addListener('success', function() {
-		me.transitioning = false;
-		me.runState(opts.success, arguments);
-	});
-
-	act.addListener('error', function() {
-		me.transitioning = false;
-		me.runState(opts.error, arguments);
-	});
-
-	me.transitioning = true;
-	act.start();
-}
-
-/**
- * Transition directly to the given state with the given action or variable.
- */
-NobleMachine.prototype.toState = function(nameOrIndex, actOrVar) {
-	var me = this;
-
-	var state = me.findState(nameOrIndex);
-
-	if (actOrVar === undefined) {
-		me.runState(state);
-	} else if (actOrVar && actOrVar.start instanceof Function) {
-		me.transition({ action: actOrVar, success: state });
-	} else {
-		me.runState(state, [actOrVar]);
-	}
-}
-
-/**
- * Transition to the next state with the given action or variable.
- */
-NobleMachine.prototype.toNext = function() {
-	var me = this;
-
-	var nextState = me.stateIndex+1;
-	var args = Array.prototype.slice.call(arguments);
-
-	if (nextState < me.stateSequence.length) {
-		me.toState.apply(me, [nextState].concat(args));
-	} else {
-		me.toComplete.apply(me, args);
-	}
-}
-
-/**
- * As above, but to the previous state.
- */
-NobleMachine.prototype.toPrev = function() {
-	var me = this;
-
-	var nextState = me.stateIndex-1;
-	var args = Array.prototype.slice.call(arguments);
-
-	if (nextState < 0) {
-		me.toError("Tried to transition earlier than the first state?!");
-	} else {
-		me.toState.apply(me, [nextState].concat(args));
-	}
-}
-
-/**
- * Transition to the current state (i.e repeat it).
- */
-NobleMachine.prototype.toRepeat = function() {
-	me.toState(me, [me.stateIndex].concat(Array.prototype.slice.call(arguments)));
-}
-
-/**
- * Transition straight to completion.
- */
-NobleMachine.prototype.toComplete = function() {
-	this.toState.apply(this, ['complete'].concat(
-		Array.prototype.slice.call(arguments)));
-}
-
-/**
- * Transition to error handler.
- */
-NobleMachine.prototype.toError = function() {
-	this.toState.apply(this, ['error'].concat(
-		Array.prototype.slice.call(arguments)));
-}
-
-/**
- * Emit success event and mark machine as finished.
- */
-NobleMachine.prototype.emitSuccess = function() {
-	this.running = false;
-    this.emit.apply(this, ['success'].concat(
-        Array.prototype.slice.call(arguments)));
-}
-
-/** 
- * Emit error event and mark machine as finished.
- */
-NobleMachine.prototype.emitError = function() {
-	this.running = false;
-	this.emit.apply(this, ['error'].concat(
-        Array.prototype.slice.call(arguments)));
-}
 
 NobleMachine.logger = { log: sys.log, warning: sys.log, error: sys.log };
 
